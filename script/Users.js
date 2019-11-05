3d608807b360b5ba0cbf378b22210769d8a23d9a
Ricart <ricarteusuo11@gmail.com>
2019-11-05 17:17:09 -0300

    iniciando função updateUser()

--- script/Users.js
+++ script/Users.js
@@ -96,23 +96,52 @@ function preencheUpdate() {
     var user = firebase.auth().currentUser;
     if (user != null) {
         user.providerData.forEach(function (profile) {
-          nome = profile.displayName;
-          email = profile.email;
+            nome = profile.displayName;
+            email = profile.email;
         });
-      }
+    }
 
     var main = document.getElementById("main");
 
     var resultado = "<form>";
     resultado+="<h3>Email</h3><input id='novoEmail' type='email' value='"+email+"'>";
     resultado+="<h3>Nome</h3><input id='novoNome' type='text' value='"+nome+"'>";
-    resultado+="<h3>Nova Senha</h3><input id='novaSenha' type='password'>"
-    resultado+="<h3>Senha Atual</h3><input id='confirmaSenha' type='password'>"
-    resultado+="<h3>Senha Atual</h3><input id='atualSenha' type='password'>"
+    resultado+="<h3>Nova Senha</h3><input id='novaSenha' type='password'>";
+    resultado+="<h3>Senha Atual</h3><input id='confirmaSenha' type='password'>";
+    resultado+="<h3>Senha Atual</h3><input id='atualSenha' type='password'>";
 
     resultado+="<div class='submit'><a id='update'>Atualizar</a></div>";
 
     resultado+="</form>"
 
     main.innerHTML = resultado;
+}
+
+// Update usuário
+function updateUser() {
+    var user = firebase.auth().currentUser;
+    if (user != null) {
+        user.providerData.forEach(function (profile) {
+            nome = profile.displayName;
+            email = profile.email;
+        });
+    }
+
+    var main = document.getElementById("main");
+
+    user.updateProfile({
+        Nome: nome,
+    }).then(function() {
+        // Update successful.
+    }).catch(function(error) {
+        window.alert("Nome não pôde ser atualizado")
+    });
+
+    user.updateEmail(email).then(function() {
+        // Update successful.
+    }).catch(function(error) {
+        window.alert("Email não pôde ser autualizado")
+    });
+
+    preencheUpdate();
 }
