4b056e7dbdf098a6636a41ddf6e9e14ebc60aa6a
Ricart <ricarteusuo11@gmail.com>
2019-11-05 19:16:30 -0300

    corrigindo inserção no banco de dados e movendo conteúdo de atualização de usuário

--- script/Users.js
+++ script/Users.js
@@ -22,24 +22,25 @@ Cadastrar.addEventListener('click', function () {
             Email : Email.value,
             Moderador: y,
             Ativo: y,
-        
+
           };
-          
-                
-          
-          let db = firebase.database().ref().child('Usuários').push(Users);
-          db.set(Users);
+
+
+
+
     firebase
     .auth()
     .createUserWithEmailAndPassword(Email.value, Senha.value)
     .then(function() {
+        let db = firebase.database().ref().child('Usuários').push(Users);
+        db.set(Users);
         alert(Email.value + " - Conta cadastrada com sucesso!");
         Nome.value="";
         Email.value="";
         Senha.value="";
         ConfirmacaodeSenha.value="";
         window.location.href = "../index.html";
-    
+
     })
     .catch(function (error) {
         // Handle Errors here.
@@ -88,72 +89,4 @@ if(Login!=null){
     });
     
     
-}
-
-
-// Preenche tela de update
-function preencheUpdate() {
-    var user = firebase.auth().currentUser;
-    if (user != null) {
-        user.providerData.forEach(function (profile) {
-            nome = profile.displayName;
-            email = profile.email;
-        });
-    }
-
-    var main = document.getElementById("main");
-
-    var resultado = "<form>";
-    resultado+="<h3>Email</h3><input id='novoEmail' type='email' value='"+email+"'>";
-    resultado+="<h3>Nome</h3><input id='novoNome' type='text' value='"+nome+"'>";
-    resultado+="<h3>Nova Senha</h3><input id='novaSenha' type='password'>";
-    resultado+="<h3>Senha Atual</h3><input id='confirmaSenha' type='password'>";
-    resultado+="<h3>Senha Atual</h3><input id='atualSenha' type='password'>";
-
-    resultado+="<div class='submit'><a id='update'>Atualizar</a></div>";
-
-    resultado+="</form>"
-
-    main.innerHTML = resultado;
-}
-
-// Update usuário
-function updateUser() {
-    var user = firebase.auth().currentUser;
-    if (user != null) {
-        user.providerData.forEach(function (profile) {
-            nome = profile.displayName;
-            email = profile.email;
-        });
-    }
-
-    var main = document.getElementById("main");
-
-    user.updateProfile({
-        Nome: nome,
-    }).then(function() {
-        // Update successful.
-    }).catch(function(error) {
-        window.alert("Nome não pôde ser atualizado")
-    });
-
-    user.updateEmail(email).then(function() {
-        // Update successful.
-    }).catch(function(error) {
-        window.alert("Email não pôde ser autualizado")
-    });
-
-    preencheUpdate();
-}
-
-function deleteUser() {
-    var user = firebase.auth().currentUser;
-
-    user.delete().then(function() {
-        window.alert("Usuário removido com sucesso!")
-        window.location.href = 'login.html'
-    }).catch(function(error) {
-        window.alert("Não foi possível remover a sua conta!,0 ")
-    });
-
 }
