d8b61303c9cb78f8947e89670e0e0c4a34096a91
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2019-11-07 18:24:58 -0300

    menu limitado

--- script/limitacao.js
+++ script/limitacao.js
@@ -2,6 +2,32 @@
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log( user );
+
+      
+    var menu = document.getElementById("menu");
+    
+    var resultado = "<div class='menu-left' ><ul>";
+    resultado+="<li><a href='index.html'>Página Inicial</a></li>"
+    resultado+="<li><a onclick='preencheUpdate()' href='#'>Minha Conta</a></li>";
+    resultado+="<li><a href=''>Minhas Reservas</a></li>"
+    resultado+="<li><a href=''>História das Reservas</a></li>";
+    resultado+="<li ><a  onclick='sair()' href='' >Sair</a></li>";
+
+    resultado+="</ul>";
+    resultado+="</div>";
+
+    menu.innerHTML = resultado;
+
+    
+    /*
+        
+        
+        
+        
+        
+          
+*/
+
     } else {
       alert("Você não está autenticado, precisa fazer o login para entrar!");
         window.location.href = "../html/login.html";
