98ad0875301a677ae5c8f748b9225b9f1f6da90f
Ricart <ricarteusuo11@gmail.com>
2019-11-09 14:02:22 -0300

    adicionando necessidade de informar a senha atual para alterar dados de usuário

--- script/UpadteUser.js
+++ script/UpadteUser.js
@@ -17,7 +17,7 @@ function preencheUpdate() {
     resultado+="<div class='update-content' id='nome'><h3>Nome</h3><input id='novoNome' type='text'></div>";
     resultado+="<div class='update-content' id='newSenha'><h3>Nova Senha</h3><input id='novaSenha' type='password'></div>";
     resultado+="<div class='update-content' id='confirm'><h3>Confirmar Nova Senha</h3><input id='confirmaSenha' type='password'></div>";
-    resultado+="<div class='update-content' id='SenhaAtual'><h3>Senha Atual</h3><input id='atualSenha' type='password'></div>";
+    resultado+="<div class='update-content' id='SenhaAtual' aria-required='true'><h3>Senha Atual</h3><input id='atualSenha' type='password'></div>";
 
     resultado+="<div class='submit'><a id='update'>salvar alterações</a></div>";
     resultado+="<div class='delete'><a id='delete'>excluir conta</a></div>";
@@ -46,62 +46,72 @@ async function insereEventoUpdate(){
     var Update = document.getElementById("update");
     var novaSenha = document.getElementById("novaSenha");
     var confirmaSenha = document.getElementById("confirmaSenha");
+    var senhaAtual = document.getElementById("atualSenha");
 
 
     Update.addEventListener('click', function () {
-        if (novaSenha.value===confirmaSenha.value) {
-            console.log("Senhas iguias")
-            var user = firebase.auth().currentUser;
-            var email;
-            // Definindo o email do usuário logado
-            if (user != null) {
-                email = user.email;
-            }
-            console.log("Email logado: "+email);
 
-            firebase.database().ref('Usuarios').on('value', function (snapshot){
-                snapshot.forEach(function (item) {
-                    console.log("Email comparado: "+item.val().Email);
-                    if (email === item.val().Email) {
-
-                        //Definição das variáveis a serem utilizadas
-                        var key = item.val().Chave;
-                        console.log("Chave: "+key);
-                        var novoNome = document.getElementById("novoNome").value;
-
-                        var novoEmail = document.getElementById("novoEmail").value;
-
-
-                        if ( novoEmail !== item.val().Email){
-                            console.log("Emails diferentes");
-                            //Atualizando email na autenticação
-                            atualizaEmail(key, novoEmail);
-                        }
-
-                        //Atualizando nome no banco de dados
-                        if (item.val().Nome !== novoNome && novoNome !== "") {
-                            console.log("Nomes diferentes");
-                            // console.log(novoNome)
-                            firebase.database().ref('/Usuarios/'+key).update({
-                                Nome: novoNome
-                            });
-                            console.log("Nome atualizado")
-                        }
-
-                        //Atualiza senha
-                        if (novaSenha.value !== "") {
-                            atualizaSenha();
-                        }
-                    }
-                });
-            });
-
-            // user.Email = document.getElementById("novoEmail").value;
-            // user.Nome = document.getElementById("novoNome").value;
-
-        } else{
-            console.log("Senhas imcompativeis!");
+        var user = firebase.auth().currentUser;
+        var email;
+        // Definindo o email do usuário logado
+        if (user != null) {
+            email = user.email;
         }
+        console.log("Email logado: "+email);
+
+        firebase.auth().signInWithEmailAndPassword(email, senhaAtual.value).then(function(result) {
+
+            if (novaSenha.value === confirmaSenha.value) {
+                console.log("Senhas iguias")
+
+                firebase.database().ref('Usuarios').on('value', function (snapshot) {
+                    snapshot.forEach(function (item) {
+                        console.log("Email comparado: " + item.val().Email);
+                        if (email === item.val().Email) {
+
+                            //Definição das variáveis a serem utilizadas
+                            var key = item.val().Chave;
+                            console.log("Chave: " + key);
+                            var novoNome = document.getElementById("novoNome").value;
+
+                            var novoEmail = document.getElementById("novoEmail").value;
+
+
+                            if (novoEmail !== item.val().Email) {
+                                console.log("Emails diferentes");
+                                //Atualizando email na autenticação
+                                atualizaEmail(key, novoEmail);
+                            }
+
+                            //Atualizando nome no banco de dados
+                            if (item.val().Nome !== novoNome && novoNome !== "") {
+                                console.log("Nomes diferentes");
+                                // console.log(novoNome)
+                                firebase.database().ref('/Usuarios/' + key).update({
+                                    Nome: novoNome
+                                });
+                                console.log("Nome atualizado")
+                            }
+
+                            //Atualiza senha
+                            if (novaSenha.value !== "") {
+                                atualizaSenha();
+                            }
+                        }
+                    });
+                });
+
+                // user.Email = document.getElementById("novoEmail").value;
+                // user.Nome = document.getElementById("novoNome").value;
+
+            } else {
+                console.log("Senhas imcompativeis!");
+            }
+        }).catch(function (error) {
+            window.alert("Senha incorreta!");
+
+        });
+        senhaAtual.value = "";
     });
 }
 
