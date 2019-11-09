437a6cac35d9e3d71cc36241d680353b1e27a3fb
Mickael <araujomickael18@gmail.com>
2019-11-09 09:27:48 -0300

    criando script do botao responsivo

---
+++ script/sidebar.js
@@ -0,0 +1,8 @@
+var menu = document.getElementById("menu");
+
+var botaoResponsivo = "<div class='responsive-button' onclick='responsiveMenu()'>";
+
+botaoResponsivo+= "<a><span></span><span></span><span></span></a>";
+botaoResponsivo+= "</div>";
+
+menu.innerHTML = botaoResponsivo;
