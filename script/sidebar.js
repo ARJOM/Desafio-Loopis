44c59ce7791e623e08a9d68a9722d6a52c1582c1
Mickael <araujomickael18@gmail.com>
2019-11-09 11:11:27 -0300

    deletando script sidebar.js

--- script/sidebar.js
+++
@@ -1,8 +0,0 @@
-var menu = document.getElementById("menu");
-
-var botaoResponsivo = "<div class='responsive-button' onclick='responsiveMenu()'>";
-
-botaoResponsivo+= "<a><span></span><span></span><span></span></a>";
-botaoResponsivo+= "</div>";
-
-menu.innerHTML = botaoResponsivo;
