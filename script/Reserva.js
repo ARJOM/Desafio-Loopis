0fee44ba4c405cc377534f7445e34b39d98d9621
Iarlyson <Iarlyson.Santana@outlook.com>
2019-11-10 00:28:49 -0200

    70% das funcionalidade da reserva

--- script/Reserva.js
+++ script/Reserva.js
@@ -1,8 +1,10 @@
+//Pagina para registar reservar
 function reservarEspaco() {
 
     var main = document.getElementById("main");
 
     var resultado="";
+    resultado+="<h2 class=\"text\">Reservar de Espaço</h2>";
     resultado+="<p class=\"text\">Data da reserva: </p>";
     resultado+="<input id='data' type=\"date\">";
     resultado+="<div class=\"content\">";
@@ -13,7 +15,7 @@ function reservarEspaco() {
     resultado+="<p class=\"text\">Espaços:</p>";
     resultado +="<select id=\"Espaco\">";
     resultado +="<option value='null'></option>"
-    //Listando tipos de espaço
+    //Listando espaços
     firebase.database().ref('Espaco').on('value', function (snapshot) {
         snapshot.forEach(function (item) {
             resultado += "<option value='"+item.val().Chave+"'>"+item.val().NomedoEspaco+"</option>";
@@ -29,7 +31,7 @@ function reservarEspaco() {
 var user = firebase.auth().currentUser;
 var email = user.email;
 
-
+//Criar uma reserva de Espaço
 function registrarReserva(){
     var user = firebase.auth().currentUser;
     var email = user.email;
@@ -67,6 +69,8 @@ function registrarReserva(){
         abrir();
 }
 
+
+//Ver todas as Reservas
 function MinhasReservas(){
 
     var main = document.getElementById("main");
@@ -80,12 +84,12 @@ function MinhasReservas(){
             firebase.database().ref('Reserva').on('value', function (snapshot) {
                 snapshot.forEach(function (item) {
                     resultado += "<tr>"
-           
                     resultado += "<td>"+ item.val().Espaco +"</td>";  
                     resultado += "<td>"+ item.val().Data +"</td>";
                     resultado += "<td>"+ item.val().HoradeInicio +"</td>";
                     resultado += "<td>"+ item.val().HoradeEntrega +"</td>";
-                    resultado += "<td><a onclick=\"editarreserva('"+item.val().Chave+"')\" value='Editar' href='#'></a></td>";
+                    resultado += "<td><a onclick=\"editarreserva('"+item.val().Chave+"')\" href='#'>Editar</a></td>";
+                    resultado += "<td><a onclick=\"deletarreserva('"+item.val().Chave+"')\" href='#'>Deletar</a></td>";
                     resultado += "</tr>";
                         });
                     });
@@ -101,43 +105,49 @@ function MinhasReservas(){
 
 }
 
-//Função para editar cada usuário individualmente;
+
+
+
+//Função para editar cada reserva individualmente;
 function editarreserva(key){
+
     var main = document.getElementById("main");
-    var resultado = "";
-    resultado += "<h2>Edição de nivel de Usuário</h2>";
-    resultado += "<form>";
-    resultado += "<p>Nome</p>";
-    resultado += "<input id='nome' disabled>";
-    resultado += "<p>Email</p>";
-    resultado += "<input id='email' disabled>";
-    resultado += "<p>Moderador</p>";
-    resultado += "<select id='moderador' >";
-    resultado +="<option value='null'></option>";
-    resultado +="<option>true</option>";
-    resultado +="<option>false</option>";
-    resultado += "</select>";
-    resultado += "<p>Administrador</p>";
-    resultado += "<select id='administrador' >";
-    resultado +="<option value='null'></option>";
-    resultado +="<option>true</option>";
-    resultado +="<option>false</option>";
-    resultado += "</select>";
-    resultado += "</form><br/>";
-    resultado += "<button id='btn' name='Editar Nivel' onclick=\"editarNivel('"+key+"')\">Editar Nivel</button><br/>";
-    resultado += "<button id='btn' name='Gerenciar Níveis' onclick='listaUser()'>Voltar para Gerenciador de Níveis </button>"
+
+    var resultado="";
+    resultado+="<h2 class=\"text\">Editar Reservar de Espaço</h2>";
+    resultado+="<p class=\"text\">Data da reserva: </p>";
+    resultado+="<input id='data' type=\"date\">";
+    resultado+="<div class=\"content\">";
+    resultado+="<p class=\"text\">Horário de início: </p>";
+    resultado+="<input type=\"time\" id=\"hora-inicio\" min=\"07:00\" max=\"22:00\" required>";
+    resultado+="<p class=\"text\">Horário de entrega: </p>";
+    resultado+="<input type=\"time\" id=\"hora-entrega\" min=\"07:00\" max=\"22:00\" required>";
+    resultado+="<p class=\"text\">Espaços:</p>";
+    resultado +="<select id=\"Espaco\">";
+    resultado +="<option value='null'></option>"
     main.innerHTML = resultado;
+    //Listando tipos de espaço
+    firebase.database().ref('Espaco').on('value', function (snapshot) {
+        snapshot.forEach(function (item) {
+            resultado += "<option value='"+item.val().Chave+"'>"+item.val().NomedoEspaco+"</option>";
+        });
+    });
+ 
 
 
-    firebase.database().ref('Usuarios').on('value', function (snapshot) {
+    firebase.database().ref('Reserva').on('value', function (snapshot) {
         snapshot.forEach(function (item) {
             if (key === item.val().Chave){
-                document.getElementById("nome").value = item.val().Nome;
-                document.getElementById("email").value = item.val().Email;
-                document.getElementById("moderador").value = item.val().Moderador;
-                document.getElementById("administrador").value = item.val().Administrador;
+                document.getElementById("data").value = item.val().Data;
+                document.getElementById("hora-inicio").value = item.val().HoradeInicio;
+                document.getElementById("hora-entrega").value = item.val().HoradeEntrega;
             }
         });
     });
 
+}
+
+function deletarreserva(key){
+firebase.database().ref('/Usuarios/'+key).remove();
+alert("Reserva deletada!");
 }
